dbccdf94cc7f12b9081321c057148a86d481570e
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-07-24 13:10:09 -0400

    Implement favorite photo feature in TopNavigation
    - Added TopNavigation component to display the navigation bar.
    - Added FavBadge component to display the heart icon.
    - Added FavIcon component to render the heart icon.
    - Implemented logic to track favorite photos and update heart icon.
    - Display notification when at least one photo is favorited (not working).

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FavIcon from './FavIcon';
-
 import '../styles/FavBadge.scss';
 
-export const FavBadge = ({ isFavPhotoExist }) => {
+export const FavBadge = ({ favouritePhotos, updateDisplayAlert, setFill }) => {
+    const [isFavPhotoExist, setIsFavPhotoExist] = useState (favouritePhotos.length > 0);
+    
+    const updateFav = () => {
+        setIsFavPhotoExist(!isFavPhotoExist);
+        updateDisplayAlert(!isFavPhotoExist); // Call the callback to update displayAlert in TopNavigation
+    };
+        
   return (
     <div className='fav-badge'>
-      <FavIcon width={20} height={17} fill="#C80000" displayAlert={!!isFavPhotoExist}/>
+      <FavIcon 
+      width={20} 
+      height={17}
+      fill={isFavPhotoExist ? "#C80000" : "#EEEEEE"}
+      displayAlert={isFavPhotoExist}
+      setDisplayAlert={updateFav}
+      setFill={setFill} // Pass down the setFill function
+      />
 
     </div>
-  ) 
+  ); 
 };
 
 export default FavBadge;
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import '../styles/TopNavigationBar.scss'
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 
-const TopNavigation = (props) => {
-  return (
-    <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList/>
-      <FavBadge />
+const TopNavigation = ({ favouritePhotos }) => {
+    const [displayAlert, setDisplayAlert] = useState(false);
+    const [fill, setFill] = useState('#EEEEEE'); // Add fill state here
+  
+    return (
+      <div className="top-nav-bar">
+        <span className="top-nav-bar__logo">PhotoLabs</span>
+        <TopicList />
+        <FavBadge 
+        favouritePhotos={favouritePhotos}
+        updateDisplayAlert={setDisplayAlert} // Pass down the setDisplayAlert function
+        setFill={setFill} // Pass down the setFill function
+      />
+       {displayAlert && <span className="notification">New favourite photo!</span>}
     </div>
-  )
-}
+    );
+};
 
 export default TopNavigation;
